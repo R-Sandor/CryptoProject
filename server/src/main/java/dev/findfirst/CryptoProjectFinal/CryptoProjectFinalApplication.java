4d33e4f6fcf8c15f9c7ebdbf7c48611481743df8
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2022-10-22 19:26:46 -0400

    Adding SpringBoot Initializer project

---
+++ server/src/main/java/dev/findfirst/CryptoProjectFinal/CryptoProjectFinalApplication.java
@@ -0,0 +1,13 @@
+package dev.findfirst.CryptoProjectFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoProjectFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoProjectFinalApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/dev/findfirst/CryptoProjectFinal/CryptoProjectFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.findfirst.CryptoProjectFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoProjectFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
